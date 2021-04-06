df4df92814a3738811b5a9862a8f43adf7fa81e0
Goksel Ozkazanc <goksel_ozkazanc_08@hotmail.com>
2021-04-06 15:13:58 +0200

    Completed

--- 03- CSS Variables/script.js
+++ 03- CSS Variables/script.js
@@ -1,5 +1,5 @@
 // ----------------------------------------------
-// * How did I code with the HELP of the course * ⬇⬇⬇⬇⬇
+// * How did I code with the HELP of the course *
 // ----------------------------------------------
 // const inputs = document.querySelectorAll(".controls input");
 
@@ -44,28 +44,26 @@ document.querySelector(".outputBlur").addEventListener("blur", changeBlur)
 document.querySelector(".outputColor").addEventListener("blur", changeColor)
 
 function changeSpace() {
-    if(document.querySelector(".outputSpacing").value > 200 || document.querySelector(".outputSpacing").value < 0){
+    if (document.querySelector(".outputSpacing").value > 200 || document.querySelector(".outputSpacing").value < 0) {
         alert("please type a number between 0 and 200")
         return
     }
     const suffix = document.querySelector("#spacing").getAttribute("data-sizing");
     document.querySelector("#spacing").value = document.querySelector(".outputSpacing").value
     document.querySelector("#img").style.padding = `${document.querySelector("#spacing").value}${suffix}`
-    
 }
 
 function changeBlur() {
-    if(document.querySelector(".outputBlur").value > 25 || document.querySelector(".outputBlur").value < 0){
+    if (document.querySelector(".outputBlur").value > 25 || document.querySelector(".outputBlur").value < 0) {
         alert("please type a number between 0 and 25")
         return
     }
     const suffix = document.querySelector("#blur").getAttribute("data-sizing");
     document.querySelector("#blur").value = document.querySelector(".outputBlur").value
     document.querySelector("#img").style.filter = `blur(${this.value}${suffix})`
- 
 }
 
-function changeColor(){ 
+function changeColor() {
     document.querySelector("#base").value = document.querySelector(".outputColor").value
     document.querySelector("#img").style.background = `${this.value}`
 
