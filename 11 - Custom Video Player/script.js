2b34639369ff38c2bc58c59d4741caa39585811a
Goksel Ozkazanc <goksel_ozkazanc_08@hotmail.com>
2021-04-26 15:21:06 +0200

    Add original project's code(commented)

--- 11 - Custom Video Player/script.js
+++ 11 - Custom Video Player/script.js
@@ -1,3 +1,6 @@
+// --------
+// MY CODE
+// --------
 bool = true
 const player = document.querySelector(".player");
 const playPauseBtn = document.querySelector(".toggle");
@@ -20,22 +23,6 @@ const current = document.querySelector(".current");
 const duration = document.querySelector(".duration");
 
 const fullScreenBtn = document.querySelector(".fullScreen")
-
-
-//displayig current time and total duration
-function currentTime() { 
-    var curmins = Math.floor(video.currentTime / 60);
-    var cursecs = Math.floor(video.currentTime - curmins * 60);
-    var durmins = Math.floor(video.duration / 60);
-    var dursecs = Math.floor(video.duration - durmins * 60);
-    if(cursecs < 10){ cursecs = `0${cursecs}`};
-    if(dursecs < 10){ dursecs = `0${dursecs}`};
-    if(curmins < 10){ curmins = `0${curmins}`};
-    if(durmins < 10){ durmins = `0${durmins}`};
-    current.textContent = `${curmins}:${cursecs}`;
-    duration.textContent = `${durmins}:${dursecs}`;
-}
-video.addEventListener("timeupdate", currentTime);
 
 
 // play pause and icon
@@ -109,8 +96,22 @@ const seek = (e) => {
 
 progressCont.addEventListener("click", seek);
 
+//displayig current time and total duration
+function currentTime() { 
+    var curmins = Math.floor(video.currentTime / 60);
+    var cursecs = Math.floor(video.currentTime - curmins * 60);
+    var durmins = Math.floor(video.duration / 60);
+    var dursecs = Math.floor(video.duration - durmins * 60);
+    if(cursecs < 10){ cursecs = `0${cursecs}`};
+    if(dursecs < 10){ dursecs = `0${dursecs}`};
+    if(curmins < 10){ curmins = `0${curmins}`};
+    if(durmins < 10){ durmins = `0${durmins}`};
+    current.textContent = `${curmins}:${cursecs}`;
+    duration.textContent = `${durmins}:${dursecs}`;
+}
+video.addEventListener("timeupdate", currentTime);
 
-//full screen
+//making full screen and hide controls
 const fullScreen = () => {
    video.requestFullscreen()
    
@@ -118,4 +119,65 @@ const fullScreen = () => {
 fullScreenBtn.addEventListener("click", fullScreen)
 video.addEventListener("dblclick", fullScreen)
 
+
+
+// ---------------------
+// ORIGINAL PROJECT CODE 
+// ---------------------
+
+/* Get Our Elements */
+// const player = document.querySelector('.player');
+// const video = player.querySelector('.viewer');
+// const progress = player.querySelector('.progress');
+// const progressBar = player.querySelector('.progress__filled');
+// const toggle = player.querySelector('.toggle');
+// const skipButtons = player.querySelectorAll('[data-skip]');
+// const ranges = player.querySelectorAll('.player__slider');
+
+// /* Build out functions */
+// function togglePlay() {
+//   const method = video.paused ? 'play' : 'pause';
+//   video[method]();
+// }
+
+// function updateButton() {
+//   const icon = this.paused ? '►' : '❚ ❚';
+//   console.log(icon);
+//   toggle.textContent = icon;
+// }
+
+// function skip() {
+//  video.currentTime += parseFloat(this.dataset.skip);
+// }
+
+// function handleRangeUpdate() {
+//   video[this.name] = this.value;
+// }
+
+// function handleProgress() {
+//   const percent = (video.currentTime / video.duration) * 100;
+//   progressBar.style.flexBasis = `${percent}%`;
+// }
+
+// function scrub(e) {
+//   const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+//   video.currentTime = scrubTime;
+// }
+
+// /* Hook up the event listeners */
+// video.addEventListener('click', togglePlay);
+// video.addEventListener('play', updateButton);
+// video.addEventListener('pause', updateButton);
+// video.addEventListener('timeupdate', handleProgress);
+
+// toggle.addEventListener('click', togglePlay);
+// skipButtons.forEach(button => button.addEventListener('click', skip));
+// ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+// ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+// let mousedown = false;
+// progress.addEventListener('click', scrub);
+// progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+// progress.addEventListener('mousedown', () => mousedown = true);
+// progress.addEventListener('mouseup', () => mousedown = false);
 
